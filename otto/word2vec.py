d6f566a0d4699a7a35ea54cf1338bc732d594529
PiotrekGa <piotrek.gabrys@gmail.com>
2022-11-23 12:40:25 +0100

    fix

--- otto/word2vec.py
+++ otto/word2vec.py
@@ -3,18 +3,19 @@ from tqdm import tqdm
 from gensim.models import Word2Vec
 
 if __name__ == '__main__':
-    df1 = pd.read_parquet('data/raw/test.parquet')
-    df2 = pd.read_parquet('data/raw/train.parquet')
+    df1 = pd.read_parquet('../data/raw/test.parquet')
+    df2 = pd.read_parquet('../data/raw/train.parquet')
     sessions = pd.concat([df1, df2])
     del df1, df2
+    sessions.aid = sessions.aid.astype(str)
     sessions_aids = sessions.groupby('session')['aid'].apply(list)
 
     x = [' '.join(sessions_aids[idx])
          for idx in tqdm(sessions_aids.index)]
-    with open('w2v_input.txt', 'w') as f:
+    with open('../data/w2v_input.txt', 'w') as f:
         for line in x:
             f.write(f"{line}\n")
 
-    model = Word2Vec(corpus_file="w2v_input.txt",
+    model = Word2Vec(corpus_file="../data/w2v_input.txt",
                      vector_size=100, window=5, min_count=1, workers=7)
-    model.save("word2vec.model")
+    model.save("../data/word2vec.model")
